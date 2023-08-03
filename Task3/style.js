7e5073752d24b8d41dec3e66b2ccb539259b2fce
Shweta Patil <125206781+shwetajpatil@users.noreply.github.com>
2023-08-03 23:08:30 +0530

    Add files via upload

---
+++ Task3/style.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box");
+const ListContainer = document.getElementById("List-Container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span); 
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+ListContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", ListContainer.innerHTML)
+}
+function showTask(){
+    ListContainer.innerHTML = localStorage.getItem("data");
+}
+
